3af7937983ecd3742daaa0e398957c56c30c719a
Aksaht <akshatgoyal6307@gmail.com>
2025-09-14 01:08:31 +0530

    added the detect fork functionality and do some code correction to make code more extensable

--- src/main/java/org/akshat/api/RuleEngine.java
+++ src/main/java/org/akshat/api/RuleEngine.java
@@ -1,93 +1,131 @@
 package org.akshat.api;
 
 import org.akshat.boards.TicTacToeBoard;
-import org.akshat.game.Board;
-import org.akshat.game.GameState;
+import org.akshat.game.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class RuleEngine {
 
-    public GameState getState(Board board) {
-        if(board instanceof TicTacToeBoard) {
-            TicTacToeBoard board1 = (TicTacToeBoard) board;
-            String firstCharacter="-";
-            boolean someoneIsVictorious = true;
-            for(int i = 0; i < 3; i++) {
-                firstCharacter = board1.getSymbol(i, 0);
-                someoneIsVictorious = firstCharacter != null;
-                if(firstCharacter != null) {
-                    for (int j = 1; j < 3; j++) {
-                        if (!firstCharacter.equals(board1.getSymbol(i, j))) {
-                            someoneIsVictorious = false;
-                            break;
-                        }
-                    }
-                }
-                if(someoneIsVictorious) {
-                    return new GameState(true, firstCharacter);
-                }
-            }
+    Map<String, List<Rule<TicTacToeBoard>>> ruleMap = new HashMap<>();
 
-
-            someoneIsVictorious = true;
-            for(int i = 0; i < 3; i++) {
-                firstCharacter = board1.getSymbol(0, i);
-                someoneIsVictorious = firstCharacter != null;
-                if (firstCharacter != null){
-                    for (int j = 1; j < 3; j++) {
-                        if (!firstCharacter.equals(board1.getSymbol(j, i))) {
-                            someoneIsVictorious = false;
-                            break;
-                        }
-                    }
-                }
-                if(someoneIsVictorious) {
-                    return new GameState(true, firstCharacter);
-                }
-            }
-
-            firstCharacter = board1.getSymbol(0, 0);
-            boolean diagComplete = firstCharacter != null;
-            for(int i = 0; i < 3; i++) {
-                if(firstCharacter != null && !firstCharacter.equals(board1.getSymbol(i, i))) {
-                    diagComplete = false;
-                    break;
-                }
-            }
-
-            if(diagComplete) {
-                return new GameState(true, firstCharacter);
-            }
-
-            firstCharacter = board1.getSymbol(0, 2);
-            boolean revDiagComplete = firstCharacter!=null;
-            for(int i = 0; i < 3; i++) {
-                if(firstCharacter != null && !firstCharacter.equals(board1.getSymbol(i, 2 - i))) {
-                    revDiagComplete = false;
-                    break;
-                }
-            }
-
-            if(revDiagComplete) {
-                return new GameState(true, firstCharacter);
-            }
-
+    public RuleEngine(){
+        ruleMap.put(TicTacToeBoard.class.getName(),new ArrayList<>());
+        ruleMap.get(TicTacToeBoard.class.getName()).add(new Rule<TicTacToeBoard>(board -> outerTraversal((i,j)->board.getSymbol(i,j))));
+        ruleMap.get(TicTacToeBoard.class.getName()).add(new Rule<TicTacToeBoard>(board -> outerTraversal((i,j)->board.getSymbol(j,i))));
+        ruleMap.get(TicTacToeBoard.class.getName()).add(new Rule<TicTacToeBoard>(board -> traverse((i) -> board.getSymbol(i,i))));
+        ruleMap.get(TicTacToeBoard.class.getName()).add(new Rule<TicTacToeBoard>(board -> traverse((i) -> board.getSymbol(i,2-i))));
+        ruleMap.get(TicTacToeBoard.class.getName()).add(new Rule<TicTacToeBoard>(board -> {
             int countOfFilledCells = 0;
             for(int i = 0; i < 3; i++) {
                 for(int j = 0; j < 3; j++) {
-                    if(board1.getSymbol(i, j) != null) {
+                    if(board.getSymbol(i, j) != null) {
                         countOfFilledCells++;
                     }
                 }
             }
             if(countOfFilledCells == 9) {
-                return new GameState(true, "");
+                return new GameState(true, "-");
+            }
+            return new GameState(false, "-");
+        }));
+    }
+    public GameInfo getInfo(Board board){
+        if(board instanceof TicTacToeBoard){
+            GameState gameState = getState(board);
+            final String[] players = new String[]{"X","O"};
+            for(int playerSymbol=0;playerSymbol<2;playerSymbol++) {
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        Board b = board.copy();
+                        Player player = new Player(players[playerSymbol]);
+                        b.move(new Move(new Cell(i, j), player));
+                        boolean canStillWin = false;
+                        for (int k = 0; k < 3; k++) {
+                            for (int l = 0; l < 3; l++) {
+                                Board b1 = b.copy();
+                                b1.move(new Move(new Cell(k, l), player.flip()));
+                                if (getState(b1).getWinner().equals(player.flip().symbol())) {
+                                    canStillWin = true;
+                                    break;
+                                }
+                            }
+                            if (canStillWin) break;
+                        }
+                        if (canStillWin) {
+                            return new GameInfoBuilder()
+                                    .isOver(gameState.isOver())
+                                    .winner(gameState.getWinner())
+                                    .hasFork(true)
+                                    .player(player)
+                                    .build();
+                        }
+                    }
+                }
+            }
+            return new GameInfoBuilder().isOver(gameState.isOver())
+                    .winner(gameState.getWinner())
+                    .build();
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public GameState getState(Board board) {
+        if(board instanceof TicTacToeBoard) {
+            TicTacToeBoard board1 = (TicTacToeBoard) board;
+            List<Rule<TicTacToeBoard>> rules = ruleMap.get(TicTacToeBoard.class.getName());
+            for(Rule<TicTacToeBoard> r:rules){
+                GameState gameState = r.condition.apply(board1);
+                if(gameState.isOver()){
+                    return gameState;
+                }
             }
             return new GameState(false, "");
-
-
         }
         else {
             return new GameState(false, "");
         }
+    }
+    private GameState outerTraversal(BiFunction<Integer,Integer,String> next){
+        GameState result = new GameState(false,"-");
+        for(int i=0;i<3;i++){
+            final int ii = i;
+            GameState traversal = traverse(j->next.apply(ii,j));
+            if(traversal.isOver()){
+                result = traversal;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private GameState traverse(Function<Integer,String> traversal){
+        GameState result = new GameState(false,"-");
+        boolean possibleStreak = true;
+        for(int j=0;j<3;j++){
+            if(traversal.apply(j)==null || !traversal.apply(0).equals(traversal.apply(j))){
+                possibleStreak = false;
+                break;
+            }
+        }
+        if(possibleStreak){
+            result= new GameState(true,traversal.apply(0));
+        }
+        return result;
+    }
+}
+
+class Rule<T extends Board>{
+    Function<T,GameState> condition;
+
+    public Rule(Function<T,GameState> condition){
+        this.condition = condition;
     }
 }
